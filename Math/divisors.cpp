f29574a6c79951ebb14bfbc739e8d3d868b3de6f
Pravallika Kotte <146621306+prav-kotte1@users.noreply.github.com>
2024-06-21 07:09:32 +0530

    D2P2

---
+++ Math/divisors.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int divisor(int n){
+    if(n < 0) cout << "negative" << endl;
+    for(int i=1 ; i<=n ; i++){
+        if(n%i == 0){
+            cout << i << " " ;
+        }
+    }
+};
+
+int main(){
+    int n;
+    cout << "Enter the number:" << endl;
+    cin >> n;
+    divisor(n);
+
+    return 0;
+}
